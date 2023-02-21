74dee32021ecb8fb83b4723f80da790a98ba0ffa
saltionsatka <saltionsatka@gmail.com>
2023-02-22 00:31:44 +0100

    Further styling with MUI Material commit

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,42 @@
+import {
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material'
+import { Product } from '../../app/models/product'
+
+interface Props {
+  product: Product
+}
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="http://picsum.photos/200"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  )
+}
