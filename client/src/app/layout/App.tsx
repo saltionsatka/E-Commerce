fe64f76f3d79a1fc6837b3974f95af80015e8e5a
saltionsatka <saltionsatka@gmail.com>
2023-02-22 21:52:30 +0100

    Simple Components Creation commit

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,39 +1,37 @@
-import { Container, CssBaseline } from '@mui/material'
-import { useEffect, useState } from 'react'
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material'
+import { useState } from 'react'
 import Catalog from '../../features/catalog/Catalog'
-import { Product } from '../models/product'
 import Header from './Header'
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:5118/api/products')
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-  }, [])
-
-  function addProduct() {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1),
-        price: prevState.length * 100 + 100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200',
+  const [darkMode, setDarkMode] = useState(false)
+  const paletteType = darkMode ? 'dark' : 'light'
+  const darkTheme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
       },
-    ])
+    },
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode)
   }
+
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <CssBaseline /> {/*Removes the margin in all browsers*/}
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog products={products} addProduct={addProduct} />
+        <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Grid, List } from '@mui/material'
+import { Grid } from '@mui/material'
 import { Product } from '../../app/models/product'
 import ProductCard from './ProductCard'
 
@@ -10,7 +10,7 @@ export default function ProductList({ products }: Props) {
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <Grid item xs={4} key={product.id}>
+        <Grid item xs={3} key={product.id}>
           <ProductCard product={product} />
         </Grid>
       ))}
