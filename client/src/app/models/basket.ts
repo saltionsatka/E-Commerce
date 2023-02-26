16a2e51c1c8041b6dd000cedb5b5d1e62c8df131
saltionsatka <saltionsatka@gmail.com>
2023-02-27 00:24:59 +0100

    Shopping Cart Addition commit

---
+++ client/src/app/models/basket.ts
@@ -0,0 +1,15 @@
+export interface BasketItem {
+  productId: number
+  name: string
+  price: number
+  pictureUrl: string
+  brand: string
+  type?: any
+  quantity: number
+}
+
+export interface Basket {
+  id: number
+  buyerId: string
+  items: BasketItem[]
+}
