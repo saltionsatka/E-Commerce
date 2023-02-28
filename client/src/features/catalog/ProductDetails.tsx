247b6d03107c11c67a10ab6afb68fac84e0895ea
saltionsatka <saltionsatka@gmail.com>
2023-02-28 01:24:07 +0100

    updating react context to redux commit

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -13,13 +13,15 @@ import Grid from '@mui/material/Grid'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import agent from '../../app/api/agent'
-import { useStoreContext } from '../../app/context/StoreContext'
 import NotFound from '../../app/errors/NotFound'
 import LoadingComponent from '../../app/layout/LoadingComponent'
 import { Product } from '../../app/models/product'
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore'
+import { removeItem, setBasket } from '../basket/basketSlice'
 
 export default function ProductDetails() {
-  const { basket, setBasket, removeItem } = useStoreContext()
+  const { basket } = useAppSelector((state) => state.basket)
+  const dispatch = useAppDispatch()
   const { id } = useParams<{ id: string }>()
   const [product, setProduct] = useState<Product | null>(null)
   /*Initial value of null, if the product doesn't exist it is null*/
@@ -48,13 +50,17 @@ export default function ProductDetails() {
     if (!item || quantity > item.quantity) {
       const updatedQuantity = item ? quantity - item.quantity : quantity
       agent.Basket.addItem(product?.id!, updatedQuantity)
-        .then((basket) => setBasket(basket))
+        .then((basket) => dispatch(setBasket(basket)))
         .catch((error) => console.log(error))
         .finally(() => setSubmitting(false))
     } else {
       const updatedQuantity = item.quantity - quantity
       agent.Basket.removeItem(product?.id!, updatedQuantity)
-        .then(() => removeItem(product?.id!, updatedQuantity))
+        .then(() =>
+          dispatch(
+            removeItem({ productId: product?.id!, quantity: updatedQuantity }),
+          ),
+        )
         .catch((error) => console.log(error))
         .finally(() => setSubmitting(false))
     }
