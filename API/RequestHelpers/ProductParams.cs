4ffeb65d6c1d0912b1cc5401db85a8af4c3d517b
saltionsatka <saltionsatka@gmail.com>
2023-03-01 23:12:58 +0100

    filter and paginate api end commit

---
+++ API/RequestHelpers/ProductParams.cs
@@ -0,0 +1,16 @@
+﻿namespace API.RequestHelpers
+{
+    public class ProductParams : PaginationParams
+    {
+        public string OrderBy { get; set; }
+
+        public string SearchTerm { get; set; }
+
+
+        public string Types { get; set; }
+
+
+        public string Brands { get; set; }
+
+    }
+}
