4365763db2bc50cd4613886a0015f3152b7657fb
saltionsatka <saltionsatka@gmail.com>
2023-03-12 22:19:07 +0100

    finished .Net Identity commit

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,34 +4,32 @@ import {
   CssBaseline,
   ThemeProvider,
 } from '@mui/material'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import Header from './Header'
 import 'react-toastify/dist/ReactToastify.css'
-import agent from '../api/agent'
-import { getCookie } from '../util/util'
 import LoadingComponent from './LoadingComponent'
 import { useAppDispatch } from '../store/configureStore'
-import { setBasket } from '../../features/basket/basketSlice'
+import { fetchBasketAsync } from '../../features/basket/basketSlice'
 import { fetchCurrentUser } from '../../features/account/accountSlice'
 
 function App() {
   const dispatch = useAppDispatch()
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const buyerId = getCookie('buyerId')
-    dispatch(fetchCurrentUser())
-    if (buyerId) {
-      agent.Basket.get()
-        .then((basket) => dispatch(setBasket(basket)))
-        .catch((error) => console.log(error))
-        .finally(() => setLoading(false))
-    } else {
-      setLoading(false)
+  const initApp = useCallback(async () => {
+    try {
+      await dispatch(fetchCurrentUser())
+      await dispatch(fetchBasketAsync())
+    } catch (error) {
+      console.log(error)
     }
   }, [dispatch])
+
+  useEffect(() => {
+    initApp().then(() => setLoading(false))
+  }, [initApp])
 
   const [darkMode, setDarkMode] = useState(false)
   const paletteType = darkMode ? 'dark' : 'light'
--- client/src/features/account/accountSlice.ts
+++ client/src/features/account/accountSlice.ts
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify'
 import agent from '../../app/api/agent'
 import { User } from '../../app/models/user'
 import { router } from '../../app/router/Routes'
+import { setBasket } from '../basket/basketSlice'
 
 interface AccountState {
   user: User | null
@@ -17,7 +18,9 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
   'account/signInUser',
   async (data, thunkAPI) => {
     try {
-      const user = await agent.Account.login(data)
+      const userDto = await agent.Account.login(data)
+      const { basket, ...user } = userDto
+      if (basket) thunkAPI.dispatch(setBasket(basket))
       localStorage.setItem('user', JSON.stringify(user))
       return user
     } catch (error: any) {
@@ -28,17 +31,19 @@ export const signInUser = createAsyncThunk<User, FieldValues>(
 
 export const fetchCurrentUser = createAsyncThunk<User>(
   'account/fetchCurrentUser',
-    async (_, thunkAPI) => {
-        thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem('user')!)))
+  async (_, thunkAPI) => {
+    thunkAPI.dispatch(setUser(JSON.parse(localStorage.getItem('user')!)))
     try {
-      const user = await agent.Account.currentUser()
+      const userDto = await agent.Account.currentUser()
+      const { basket, ...user } = userDto
+      if (basket) thunkAPI.dispatch(setBasket(basket))
       localStorage.setItem('user', JSON.stringify(user))
       return user
     } catch (error: any) {
       return thunkAPI.rejectWithValue({ error: error.data })
     }
-    },
-  
+  },
+
   {
     condition: () => {
       if (!localStorage.getItem('user')) return false
@@ -54,30 +59,27 @@ export const accountSlice = createSlice({
       state.user = null
       localStorage.removeItem('user')
       router.navigate('/')
-      },
-      setUser: (state, action) => {
-          state.user = action.payload;
-      }
+    },
+    setUser: (state, action) => {
+      state.user = action.payload
+    },
   },
-    extraReducers: (builder) => {
-        builder.addCase(fetchCurrentUser.rejected, (state) => {
-            state.user = null;
-            localStorage.removeItem('user');
-            toast.error('Session expire - please login again');
-            router.navigate('/');
-      })
+  extraReducers: (builder) => {
+    builder.addCase(fetchCurrentUser.rejected, (state) => {
+      state.user = null
+      localStorage.removeItem('user')
+      toast.error('Session expire - please login again')
+      router.navigate('/')
+    })
     builder.addMatcher(
       isAnyOf(signInUser.fulfilled, fetchCurrentUser.fulfilled),
       (state, action) => {
         state.user = action.payload
       },
     )
-    builder.addMatcher(
-      isAnyOf(signInUser.rejected),
-      (state, action) => {
-        console.log(action.payload)
-      },
-    )
+    builder.addMatcher(isAnyOf(signInUser.rejected), (state, action) => {
+      throw action.payload;
+    })
   },
 })
 
